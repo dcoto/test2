0d3f42db418632d05a09c85305424c0db4cecbae
David <131292839+dcoto@users.noreply.github.com>
2026-02-09 21:46:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -36,29 +36,6 @@ noBtn.addEventListener("mouseover", () => {
     noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
 });
 
-// Logic to make YES btn to grow
-
-// let yesScale = 1;
-
-// yesBtn.style.position = "relative"
-// yesBtn.style.transformOrigin = "center center";
-// yesBtn.style.transition = "transform 0.3s ease";
-
-// noBtn.addEventListener("click", () => {
-//     yesScale += 2;
-
-//     if (yesBtn.style.position !== "fixed") {
-//         yesBtn.style.position = "fixed";
-//         yesBtn.style.top = "50%";
-//         yesBtn.style.left = "50%";
-//         yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
-//     }else{
-//         yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
-//     }
-// });
-
-// YES is clicked
-
 yesBtn.addEventListener("click", () => {
     title.textContent = "Gracias a ti y a tus ojos";
 
@@ -73,9 +50,9 @@ yesBtn.addEventListener("click", () => {
 
 
 // 🔒 TIME LOCK
-//const unlockDate = new Date("2026-02-14T00:00:00").getTime();
+const unlockDate = new Date("2026-02-14T00:00:00").getTime();
 //test
-const unlockDate = new Date(Date.now() + 5000).getTime();
+//const unlockDate = new Date(Date.now() + 5000).getTime();
 
 const lock = document.getElementById("lock-container");
 const content = document.getElementById("content");
